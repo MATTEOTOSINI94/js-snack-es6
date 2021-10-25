3186e54461fd35d915aaa6d618790f97cf569ff8
MATTEOTOSINI94 <matteotosini300@icloud.com>
2021-10-25 21:47:17 +0200

    mod

--- js/master.js
+++ js/master.js
@@ -1,21 +1,27 @@
-const sellBike = [
+// Snack 1
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
+
+
+const sellBike = 
+[
     { nome: "Mountain bike",
      peso:40,
      immagine: "1.jpg"
     },
-     {
+    {
         nome: "Cross bike",
         peso:20,
         immagine:"2.jpg"
-     },
-     {
+    },
+    {
         nome: "Ride bike",
         peso:70,
         immagine:"3.jpg"
-     }
+    }
 
 ]
-
 const cardContainer = document.querySelector(".card-container")
 
 let eavyBike = sellBike[0]
@@ -27,10 +33,6 @@ for (let x = 0; x < sellBike.length; x++) {
     if (eavyBike.peso > bike.peso) {
         eavyBike=bike
     }
-    
-//    let peso = eavyBike.peso
-//    let nome = eavyBike.nome
-
    const {peso,nome} = eavyBike
    
 }
@@ -46,6 +48,74 @@ cardContainer.innerHTML+=
    <p class="text-center">${peso} kg</p>
 </div>
 </div>`;
+
+
+
+// Snack2
+// Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+// Generare numeri random al posto degli 0 nelle proprietà:
+// Punti fatti e falli subiti.
+// Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+const footballTeam =
+[
+        { nome: "AC.Milan",
+        falli:0,
+        cartellini: 0,
+        },
+    {
+        nome: "Inter",
+        falli: 0,
+        cartellini: 0,
+    },
+    {
+        nome: "Napoli",
+        falli:0,
+        cartellini:0,
+    }
+]
+
+
+for (let x = 0; x < footballTeam.length; x++) {
+    const singolaSquadra = footballTeam[x];
+
+    singolaSquadra.falli =  Math.round(Math.random() * 100);
+    singolaSquadra.cartellini =  Math.round(Math.random() * 100);
+    
+
+}
+
+console.log(footballTeam)
+
+
+
+function array(footballTeam) {
+
+    let arrayVuoto ={}
+    for (let a = 0; a < footballTeam.length; a++) {
+        const {nome, cartellini} = footballTeam[a];
+
+        if (!arrayVuoto[nome]) {
+            arrayVuoto[nome]=[]  
+        }
+        arrayVuoto[nome].push({nome,cartellini})
+
+    }
+
+    return arrayVuoto
+
+}
+
+ const gruppoTeam = array(footballTeam)
+ console.log(gruppoTeam)
+
+
+
+
+
+
+
 
     // const {peso} = bike
     // console.log(peso)
